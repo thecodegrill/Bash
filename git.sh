89ecb606b59a0c4a9e25ad13e125cfd3b2dd3ac3
TheCodeGrill <thecodegrill@gmail.com>
2014-07-20 19:54:45 +0200

    TheCodeGrill: Confirmacion al ejecutar Git Push: sin día

--- git.sh
+++ git.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-day=$(date +"%A")
 if [ $1 == "push" ];
 then
   read -p "Are you sure? (y/n)" yn
   case $yn in
   [Yy]* )
-    if [ $day == "Wednesday" ];
-    then
-      read -p "Remember... today is WEDNESDAY"
-    fi
-  git "$@"
+    git "$@"
   esac
 else
   git "$@"
